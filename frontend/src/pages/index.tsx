4083dc45b7cc415228f5cd5c1c49a064138f1316
lululala <et3216@gmail.com>
2022-03-31 21:16:51 +0800

    frontend add rollbar

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,19 +2,30 @@ import { history } from 'umi';
 import { Button, Typography } from 'antd';
 import style from './index.less';
 import React from "react";
+import { Provider, ErrorBoundary } from '@rollbar/react';
+
+const rollbarConfig = {
+  accessToken: process.env.ROLLBAR_KEY,
+  environment: process.env.NODE_ENV,
+  enable: process.env.NODE_ENV === 'production',
+}
 
 export default function IndexPage() {
   return (
-    <div className={style.content}>
-      <Typography.Title>HOMEPAGE</Typography.Title>
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <div className={style.content}>
+          <Typography.Title>HOMEPAGE</Typography.Title>
 
-      <Button onClick={() => history.push('/auth/signup')}>
-        Sign Up
-      </Button>
+          <Button onClick={() => history.push('/auth/signup')}>
+            Sign Up
+          </Button>
 
-      <Button onClick={() => history.push('/auth/login')}>
-        Login
-      </Button>
-    </div>
+          <Button onClick={() => history.push('/auth/login')}>
+            Login
+          </Button>
+        </div>
+      </ErrorBoundary>
+    </Provider>
   );
 }
